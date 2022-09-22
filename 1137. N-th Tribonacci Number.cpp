d21936b8830a84e39aa56fab625cd54663d2b299
TarunKhanduri <tarunkhanduri007@gmail.com>
2022-09-22 19:44:42 +0530

    solved

---
+++ 1137. N-th Tribonacci Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        int a=0,b=1,c=1;
+        if(n==0)
+            return 0;
+        else if(n==1||n==2)
+            return 1;
+        int d;
+        while(n-->2)
+        {
+            d=a+b+c;
+            a=b;
+            b=c;
+            c=d;
+        }
+        
+        return d; 
+    }
+};
